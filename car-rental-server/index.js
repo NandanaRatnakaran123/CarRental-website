a62f4eec09b341af3b9f1bb6caa7a6d554d4eca5
NandanaRatnakaran123 <nandanaratna63@gmail.com>
2025-06-20 14:56:45 +0530

    updated index.js

--- car-rental-server/index.js
+++ car-rental-server/index.js
@@ -4,18 +4,22 @@ import cors from 'cors';
 import connectDB from './config/Db.js';
 import { clerkMiddleware } from '@clerk/express'
 import clerkwebhook from './controller/clerkwebhooks.js';
+import bodyParser from 'body-parser';
 
 connectDB()
 
 const app=express()
 app.use(cors())
 
+
+// Apply middleware *before* JSON parsing// api to listen webhook
+app.post('/api/clerk', bodyParser.raw({ type: 'application/json' }), clerkwebhook);
+
 // middlewares
 app.use(express.json())
 app.use(clerkMiddleware())
 
-// api to listen webhook
-app.use('/api/clerk',clerkwebhook)
+
 
 const PORT=3000;
 app.listen(PORT,() =>console.log(`Server running on port ${PORT} successfully`));

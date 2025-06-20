09abb045ede9987502d38a46d909cf8cd3024354
NandanaRatnakaran123 <nandanaratna63@gmail.com>
2025-06-20 15:04:28 +0530

    updated userschema.js

--- car-rental-server/model/userSchema.js
+++ car-rental-server/model/userSchema.js
@@ -1,33 +1,32 @@
 import mongoose from "mongoose";
 
-const userSchema=mongoose.Schema({
-    _id:{
-        tpye :string ,
-        required :true
+const userSchema = mongoose.Schema({
+    _id: {
+        type: String,
+        required: true
     },
-    username:{
-        type:string,
-        required:true
+    username: {
+        type: String,
+        required: true
     },
-    email:{
-        type:string,
-        required:true
+    email: {
+        type: String,
+        required: true
     },
-    image:{
-        type:string,
-        required:true
+    image: {
+        type: String,
+        required: true
     },
-    role:{
-        type:string,
-        enum:['user','owner'],default:'user'
-        
+    role: {
+        type: String,
+        enum: ['user', 'owner'],
+        default: 'user'
     },
-    recentSearchCities:[{
-        type:string,
-        required:true
+    recentSearchCities: [{
+        type: String,
+        required: true
     }]
-    
 });
 
-const User=mongoose.model('user',userSchema);
-export default User
+const User = mongoose.model('user', userSchema);
+export default User;
